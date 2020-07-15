0c4123419f8641bc054282252636f64b2948dc5e
Rahul V R <rahulwg@gmail.com>
2020-07-15 21:03:37 +0800

    Test updates

--- src/App.test.js
+++ src/App.test.js
@@ -4,13 +4,13 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Hello There 1/i);
+  const linkElement = getByText(/Welcome/i);
   expect(linkElement).toBeInTheDocument();
 });
 
 
 test('renders learn react link2', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Hello There 1/i);
+  const linkElement = getByText(/I was changeed here/i);
   expect(linkElement).toBeInTheDocument();
 });
