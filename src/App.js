c944a4c3283d71075e186ad483c1fb28ebc347f6
Rahul V R <rahulwg@gmail.com>
2020-07-15 20:52:51 +0800

    code update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-intro">Welcome</h1>
+        <h1 className="App-title">Welcome</h1>
       </header>
       <p className="App-intro">
         I was changeed here
